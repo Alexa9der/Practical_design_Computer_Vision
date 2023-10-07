227c2b2cc0d485054246f3b217860f21f3e2414b
Alex <sashakaloshh@gmail.com>
2023-10-07 09:41:32 +0200

    add lib

---
+++ lib/import_libraries.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import os
+
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+
+from tensorflow.keras.layers import Input, Dense, Conv2D, BatchNormalization
+from tensorflow.keras.layers import Dropout, MaxPooling2D, Flatten, Activation
+from tensorflow.keras import Model
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
---
+++ lib/project_functions.py
@@ -0,0 +1,3 @@
+from  project_functions.funktion_fot_visualization import *
+from  project_functions.functions_for_EDA import *
+from  project_functions.functions_for_processing_data import *
